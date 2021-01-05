3dcf9171dc2ee63682cd15bdcf4eda54ec39528e
Chris Doykov <swat1983@abv.bg>
2021-01-05 15:42:46 +0200

    added CFE page

--- src/app/experts/experts.component.ts
+++ src/app/experts/experts.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-experts',
-  templateUrl: './experts.component.html',
-  styleUrls: ['./experts.component.scss']
+  selector: "app-experts",
+  templateUrl: "./experts.component.html",
+  styleUrls: ["./experts.component.scss"],
 })
 export class ExpertsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    document.title = "Call For Experts | HUBCAP";
+    document.getElementById("hero-title").textContent = "Call For Experts";
   }
 
+  openApplyPage() {
+    window.open("https://www.f6s.com/hubcapcall1-2pull/apply", "_blank");
+  }
 }

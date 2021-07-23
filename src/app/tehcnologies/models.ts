956bbf9bff324fe7a4383007495b6e346edcdd27
Chris Doykov <swat1983@abv.bg>
2021-07-23 13:30:02 +0100

    extracted models and tools into separate .ts files

---
+++ src/app/tehcnologies/models.ts
@@ -0,0 +1,126 @@
+export const models = [
+  {
+    type: "model",
+    logo: "https://cyberarch.eu/wp-content/uploads/2020/06/Logo_cyberarch.png",
+    asset_name: "RCADE",
+    company_name: "Cyberarch Consulting",
+    url: "https://cyberarch.eu",
+    description:
+      'In the Cyber Security market there are a lot of tools for various operations and the end-user needs to select two or more tools based on their requirements. Having this in mind Cyberarch Consulting came up with an idea of creating a single product that can serve as an all-purpose tool. RCADE which is being developed as an "All in one Security Solution". It integrates Vulnerability Assessment, Log Analysis, Malware Analysis, Security Monitoring, OSINT Gathering, Hybrid Pentest Request. as one product. Moreover, the tool is available in the cloud such as AWS, Azure, Alibaba and deployable on premises. The tool is built in such a way that even a novice user can easily use it efficiently without prior training/knowledge.',
+    showMore: false, // Always leave at false
+    domains: [
+      "Energy",
+      "Retail",
+      "Banking/Financial",
+      "Government",
+      "Education",
+      "Health",
+      "Continuous Integration Testing",
+    ],
+    modelling_lang_tool: [
+      "Python",
+      "Golang",
+      "YAML",
+      "Node.js",
+      "Django",
+      "(Under development MVP will be ready in March 2021)",
+    ],
+    license_text: "Pay-as-you-go",
+    email: "info@cyberarch.eu",
+  },
+  {
+    type: "model",
+    logo: "../../assets/res/images/prosim-sample.png",
+    asset_name: "PROSIM T+",
+    company_name: "Asti Automation",
+    url: "https://www.astiautomation.ro",
+    description:
+      "PROSIM T+ is an industrial process simulator for digital manufacturing, which enables simulations of discrete and continuous industrial processes for PLC programming training with realistic signal interfaces.",
+    showMore: false, // Always leave at false
+    domains: ["Education and training"],
+    modelling_lang_tool: ["C#"],
+    license_text: "Pay-as-you-go",
+    email: "office@astiautomation.ro",
+  },
+  {
+    type: "model",
+    logo: "../../assets/res/logos/logo_brightmerge.jpg",
+    asset_name: "EezyGrid",
+    company_name: "Brightmerge",
+    url: "https://www.brightmerge.com",
+    description:
+      "Eezygrid models and optimizes the configuration and economics of renewable energy microgrid systems while factoring reliability, sustainability and resiliency parameters. The Brightmerge platform dramatically reduces the time and cost required to design projects. Brightmerge achieves this by integrating data sets such as weather, geospatial, consumption profiles, generation, and system components into one unified economic model. Brightmerge’s innovation is its AI engine for economic optimization of renewable energy, advanced energy storage and electric vehicle charging adoption.",
+    showMore: false, // Always leave at false
+    modelling_lang_tool: ["xGraph"],
+    domain: "Energy Systems Modelling",
+    license_text: "TBD",
+    email: "daniel@brightmerge.com",
+  },
+  {
+    type: "model",
+    logo: "../../assets/res/logos/shipdetectionlogo3.JPG",
+    asset_name:
+      "Automatic object detection using very high resolution satellite imagery",
+    company_name: "Lelier BV (Shipdetection)",
+    url: "https://shipdetection.eu/",
+    description:
+      "By combining multiple sources such as medium and very high resolution Optical and Synthetic aperture radar (SAR) imagery that we analyse through machine learning and automatic object detection algorithms we speed-up a monotonous task that would otherwise take weeks to now only hours. For instance It is now possible to detect and cross-reference vessels with AIS signals, ship databases and to track these vessels 24/7 across nation borders and detect illegal fishing, which otherwise would not be possible due to the limited coverage, revisit times and vessel name changes.",
+    showMore: false, // Always leave at false
+    modelling_lang_tool: ["Python"],
+    domain: "Earth Observation",
+    license_text: "Nontransferable free license",
+    email: "info@shipdetection.eu",
+  },
+  {
+    type: "model",
+    logo: "../../assets/res/logos/smm_inv_logo.png",
+    asset_name: "Water and air systems models",
+    company_name: "SMM INVEST",
+    url: "https://imobiliarproiect.ro/",
+    description:
+      "Our customized models serve to identify the optimum performance of the system and can be used in order to plan the optimal solution for projects.",
+    showMore: false, // Always leave at false
+    modelling_lang_tool: ["Custom for each model"],
+    domain: "Fluid flow",
+    license_text: "Specific for each project",
+    email: "iostoica@gmail.com",
+  },
+  {
+    type: "model",
+    dihAsset: true,
+    logo: "../../assets/res/logos/fortissimo.png",
+    asset_name: "fortissimo",
+    company_name: "fortiss",
+    url: "https://www.fortiss.org/",
+    description:
+      "The fortissimo rover shows how model-based systems engineering can be employed to design, validate and transfer software and system architectures for autonomous vehicles to an operational cyber-physical system. The fortissimo platooning model was implemented in the AutoFOCUS3 open source systems engineering tool developed by fortiss. It represents an autonomous driving function that permits automobiles or trucks to drive behind one another at extremely close distances, thus reducing fuel consumption. The model provides the logical architecture and the behaviour specification of platooning functions such as joining or leaving a platoon and car-to-car communication, as well as ADAS functions like adaptive cruise control systems and lane/emergency braking assistants. The developed functions can be implemented via code generators in the physical fortissimo Rover, a 1:10 scale model vehicle equipped with sensor technology such as cameras, ultrasound and laser-based distance meters. The virtual fortissimo platform is intended of the early validation of the models in a functional simulation environment. By means of the AutoFOCUS3 FMI interface, the vehicle behavior is co-simulated with the vehicle dynamics and the environment (modeled using other open source tools such as OpenModelica, ROS and Gazebo) and represented in a 3D simulation. At fortiss, the fortissimos are used to examine current research issues. These include a model-based method for deriving so-called assurance cases for validating the functional safety of the vehicles in line with ISO-26262, degradation and reconfiguration strategies for safeguarding critical driving functions (e.g., against hardware faults), analytical and simulation-based processes for dimensioning and validating the vehicle hardware and software architecture (i.e, design-space exploration such as HW/SW deployments).",
+    showMore: false, // Always leave at false
+    modelling_lang_tool: ["AutoFOCUS3"],
+    domain: "Automotive",
+    license_text: "© 2020 fortiss GmbH, released under a 2-Clause BSD License",
+    license_url: "https://opensource.org/licenses/BSD-2-Clause",
+    download_link:
+      "https://www.fortiss.org/en/research/living-lab/detail/fortissimo",
+    email: "mbse@fortiss.org",
+  },
+  {
+    type: "model",
+    logo: "../../assets/res/logos/logo_scubic.jpg",
+    asset_name: "SCUBIC - SMART DIGITAL TWINS",
+    company_name: "SCUBIC",
+    url: "https://scubic.tech/en/",
+    description:
+      "Our model:#1 - Predicts water and energy consumption based on historical data from a water distribution network. #2 - Input network data (size o tanks and its location, pumps power and flow capability, valves, energy price). #3 - The out come is how and when you should operate the network in order to have the minimal costs to transport water.",
+    showMore: false, // Always leave at false
+    modelling_lang_tool: [
+      "EPANET",
+      "Python",
+      "SciKit Learn and Keras",
+      "Influx DB",
+      "Docker",
+    ],
+    domain: "Applicable to any",
+    license_text: "None (Public domain)",
+    email: "info@scubic.pt",
+  },
+];

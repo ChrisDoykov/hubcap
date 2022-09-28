6723205a832eb33985018a6d80bb5a5a5050a663
Ken Pierce <kgpierce@gmail.com>
2022-09-28 22:04:17 +0100

    Small fix removing spurious dependencies for success stories.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
-import { SafePipeModule } from 'safe-pipe';
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
--- src/app/success-story/success-story.component.ts
+++ src/app/success-story/success-story.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { stories } from "../success-stories-list/success-stories";
-import { SafePipeModule } from 'safe-pipe';
 
 @Component({
   selector: "app-success-story",

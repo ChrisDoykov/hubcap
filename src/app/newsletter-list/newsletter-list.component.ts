55062393d785a241483a4b4651e026193ceb8abe
anirban59 <A.Bhattacharyya@ncl.ac.uk>
2022-11-01 21:08:15 +0000

    Added reviewed newsletter for October 2022.

--- src/app/newsletter-list/newsletter-list.component.ts
+++ src/app/newsletter-list/newsletter-list.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class NewsletterListComponent implements OnInit {
   constructor() {}
   newsletters = [
-/*
+
     {
       month: "October 2022",
       description: `Find out the latest updates in HUBCAP, learn about the outcomes of our second EXPERIMENT call in our October 2022 edition of our newsletter!`,
@@ -17,7 +17,6 @@ export class NewsletterListComponent implements OnInit {
       thumbnailAlt: "October 2022 Newsletter Front Page",
       modalTarget: "N7",
     },
-*/
     {
       month: "June 2022",
       description: `Find out the latest updates in HUBCAP, learn about the outcomes of our first EXPERIMENT call and find out more on how to apply to Call #1.5 in our June 2022 edition of our newsletter!`,
